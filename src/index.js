ae494ffcac2577bad89cccae2c9e3c9638ba90c9
Noah Appleby <appleby.n@husky.neu.edu>
2018-04-14 19:33:15 -0400

    fix webgl detector

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var THREE = require('three');
+var Detector = require('./detector');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
